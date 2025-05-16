c550f56d29e036a685ca850f95450a6affeb9f65
Sarah <sarah.wolf@htldornbirn.at>
2025-05-16 10:29:46 +0200

    fetch working and use state used - Cards are generated

--- FootballAPI/src/components/TeamContainer.tsx
+++ FootballAPI/src/components/TeamContainer.tsx
@@ -1,17 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import TeamCard from './TeamCard'
 
+
+
+interface Team {
+  teamID: number
+  teamName: string
+  shortName: string
+  teamIconUrl: string
+}
 type Props = {}
 
 export default function TeamContainer({}: Props) {
+
+  const [teams, setTeams] = useState<Team[]>([])
+
+  // Fetch data from the API
+   useEffect(() => {
+    fetch('https://api.openligadb.de/getbltable/bl1/2024').then((response) => {
+        response.json().then((data) => {
+            setTeams(data);
+        });
+      });
+
+   },[]);
+
+
+
   return (
     <div>
     <div className='grid grid-cols-3 gap-4 w-full'>
-        <TeamCard name='Manchester City' shortName='MCI' icon='https://picsum.photos/200/300'/>
-        <TeamCard name='Manchester City' shortName='MCI' icon='https://picsum.photos/200/300'/>
-        <TeamCard name='Manchester City' shortName='MCI' icon='https://picsum.photos/200/300'/>
-        <TeamCard name='Manchester City' shortName='MCI' icon='https://picsum.photos/200/300'/>
-        <TeamCard name='Manchester City' shortName='MCI' icon='https://picsum.photos/200/300'/>
+      {teams.map((team: any) => (
+        <TeamCard key={team.teamID} name={team.teamName} shortName={team.shortName} icon={team.teamIconUrl} />
+        ))}  
     </div>
     </div>
   )
